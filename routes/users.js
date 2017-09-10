21cc94de234b53082f59f361e7e0f61a63ded74b
DanielVG <danielveragi@gmail.com>
2017-09-10 08:03:08 +0200

    added simple validation

--- routes/users.js
+++ routes/users.js
@@ -11,4 +11,32 @@ router.get('/login', function(req, res){
   res.render('login');
 });
 
+// add register route
+router.post('/register', function(req, res){
+  var name = req.body.name;
+  var email = req.body.email;
+  var username = req.body.username;
+  var password = req.body.password;
+  var password2 = req.body.password2;
+
+  //get some vlaidation
+  req.checkBody("name", "Name is required").notEmpty();
+  req.checkBody("email", "Email is required").notEmpty();
+  req.checkBody("email", "Email is valid").isEmail();
+  req.checkBody("username", "Username is required").notEmpty();
+  req.checkBody("password", "password is required").notEmpty();
+  req.checkBody("password2", "Passwords does not match").equals(req.body.password);
+
+
+  //check for some error
+  var errors = req.validationErrors();
+  //check if there are any errors
+  if (errors) {
+    //if there are errors re-render the form
+    res.render('register', {errors:errors});
+  } else {
+    console.log('PASSED');
+  }
+});
+
 module.exports = router;
