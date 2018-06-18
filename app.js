c2495e5c2ad3561548a06d800d69db94dbf10e8e
danielVg <danielveragi@gmail.com>
2018-06-18 20:01:04 +0200

    code quality
    Signed-off-by: danielVg <danielveragi@gmail.com>

--- app.js
+++ app.js
@@ -1,50 +1,50 @@
 // require different packages
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var exphbs = require('express-handlebars');
-var expressValidator = require('express-validator');
-var flash = require('connect-flash');
-var session = require('express-session');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const exphbs = require("express-handlebars");
+const expressValidator = require("express-validator");
+const flash = require("connect-flash");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const mongo = require("mongodb");
+const mongoose = require("mongoose");
 
 // connect to mongoose
-mongoose.connect('mongodb://localhost/loginapp', { useMongoClient: true });
-var db = mongoose.connection;
+mongoose.connect("mongodb://localhost/loginapp", { useMongoClient: true });
+const db = mongoose.connection;
 
 // making the routes for the web page
-var routes = require('./routes/index');
-var users = require('./routes/users');
+const routes = require("./routes/index");
+const users = require("./routes/users");
 
 // initialize app
-var app = express();
+const app = express();
 
 // view engine
 // set folder to handle the views
 // tell express which engine to use
-app.set('views', path.join(__dirname, 'views'));
+app.set("views", path.join(__dirname, "views"));
 // Tell Express that for files with extension html you would like to call the exphbs function to render them
-app.engine('handlebars', exphbs({defaultLayout : 'layout'}));
-app.set('view engine', 'handlebars');
+app.engine("handlebars", exphbs({ defaultLayout: "layout" }));
+app.set("view engine", "handlebars");
 
-//Body pareser middleware
+// Body pareser middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // set static folder to hold style sheets, images, jquery
 // stuff that is public to the browser
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // middleware for Express session
 app.use(session({
 	secret: "secret",
 	saveUninitialized: true,
-	resave: true
+	resave: true,
 }));
 
 // Passport init
@@ -53,8 +53,8 @@ app.use(passport.session());
 
 // middleware for the Express validator
 app.use(expressValidator({
-  errorFormatter: function(param, msg, value) {
-      var namespace = param.split('.')
+	errorFormatter(param, msg, value) {
+      let namespace = param.split('.')
       , root    = namespace.shift()
       , formParam = root;
 
@@ -66,7 +66,7 @@ app.use(expressValidator({
       msg   : msg,
       value : value
     };
-  }
+  },
 }));
 
 // connect to the flash middleware
@@ -75,7 +75,7 @@ app.use(flash());
 // create global variables for flash messages
 // Glob. Messages
 // Global Vars
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
   res.locals.error = req.flash('error');
@@ -88,15 +88,15 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // some middleware for the routes
-app.use('/', routes);
-app.use('/users', users);
+app.use("/", routes);
+app.use("/users", users);
 
 // set the port and start the server
 
 // set the port
-app.set('port', (process.env.PORT || 3000));
+app.set("port", (process.env.PORT || 3000));
 
 // listen to port
-app.listen(app.get('port'), function(){
+app.listen(app.get("port"), () => {
 	console.log('Server started at port' + app.get('port'));
 });
